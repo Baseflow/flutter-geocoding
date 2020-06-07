12fe54872c597bf184eb525f21a99eef8d0de7ea
Maurits van Beusekom <maurits@baseflow.com>
2020-06-07 17:17:29 +0200

    Added iOS implementation

---
+++ geocoding/ios/Classes/Extensions/CLPlacemarkExtensions.m
@@ -0,0 +1,63 @@
+//
+//  CLPlacemarkExtensions.m
+//  geocoding
+//
+//  Created by Maurits van Beusekom on 07/06/2020.
+//
+
+#import <AddressBook/AddressBook.h>
+#import <Contacts/Contacts.h>
+#import "CLPlacemarkExtensions.h"
+
+@implementation CLPlacemark (CLPlacemarkExtensions)
+
+- (NSDictionary *)toPlacemarkDictionary {
+    NSString* street = @"";
+
+    if (@available(iOS 11.0, *)) {
+        if (self.postalAddress != nil) {
+            street = self.postalAddress.street;
+        }
+    } else if (@available(iOS 5.0, *)) {
+        if (self.addressDictionary != nil) {
+            street = [[self addressDictionary] objectForKey:(NSString *)kABPersonAddressStreetKey];
+        }
+    }
+    
+    NSMutableDictionary<NSString *, NSObject *> *dict = [[NSMutableDictionary alloc] initWithDictionary:@{
+        @"name": self.name == nil ? @"" : self.name,
+        @"street": street,
+        @"isoCountryCode": self.ISOcountryCode == nil ? @"" : self.ISOcountryCode,
+        @"country": self.country == nil ? @"" : self.country,
+        @"thoroughfare": self.thoroughfare == nil ? @"" : self.thoroughfare,
+        @"subThoroughfare": self.subThoroughfare == nil ? @"" : self.subThoroughfare,
+        @"postalCode": self.postalCode == nil ? @"" : self.postalCode,
+        @"administrativeArea": self.administrativeArea == nil ? @"" : self.administrativeArea,
+        @"subAdministrativeArea": self.subAdministrativeArea == nil ? @"" : self.subAdministrativeArea,
+        @"locality": self.locality == nil ? @"" : self.locality,
+        @"subLocality": self.subLocality == nil ? @"" : self.subLocality,
+    }];
+    
+    return dict;
+}
+
+- (NSDictionary *)toLocationDictionary {
+    if (self.location == nil) {
+        return nil;
+    }
+    
+    NSMutableDictionary<NSString *, NSObject*> *dict = [[NSMutableDictionary alloc] initWithDictionary:@{
+        @"latitude": @(self.location.coordinate.latitude),
+        @"longitude": @(self.location.coordinate.longitude),
+        @"timestamp": @([CLPlacemark currentTimeInMilliSeconds: self.location.timestamp]),
+    }];
+    
+    return dict;
+}
+
++ (double)currentTimeInMilliSeconds:(NSDate *)dateToConvert {
+    NSTimeInterval since1970 = [dateToConvert timeIntervalSince1970];
+    return since1970 * 1000;
+}
+
+@end
---
+++ geocoding/ios/Classes/GeocodingHandler.m
@@ -0,0 +1,133 @@
+//
+//  GeocodingHandler.m
+//  geocoding
+//
+//  Created by Maurits van Beusekom on 07/06/2020.
+//
+
+#import "GeocodingHandler.h"
+
+@implementation GeocodingHandler {
+    CLGeocoder* _geocoder;
+}
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        _geocoder = [[CLGeocoder alloc] init];
+    }
+    
+    return self;
+}
+
+- (void) geocodeFromAddress: (NSString *)address
+                     locale: (NSLocale *)locale
+                    success: (GeocodingSuccess)successHandler
+                    failure: (GeocodingFailure)failureHandler {
+    
+    if (address == nil) {
+        failureHandler(@"ARGUMENT_ERROR", @"Please supply a valid string containing the address to lookup");
+        return;
+    }
+    
+    if (@available(iOS 11.0, *)) {
+        [_geocoder geocodeAddressString:address
+                               inRegion:nil
+                        preferredLocale:locale
+                      completionHandler:^(NSArray< CLPlacemark *> *__nullable placemarks, NSError *__nullable error)
+         {
+            [GeocodingHandler completeGeocodingWith:placemarks
+                                              error:error
+                                            success:successHandler
+                                            failure:failureHandler];
+        }];
+    } else {
+        NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
+        NSArray<NSString * > *defaultLanguages;
+        
+        if (locale != nil) {
+            defaultLanguages = [standardUserDefaults arrayForKey:@"AppleLanguages"];
+            [standardUserDefaults setValue:[GeocodingHandler languageCode:locale]
+                                    forKey:@"AppleLanguages"];
+        }
+        
+        [_geocoder geocodeAddressString:address
+                      completionHandler:^(NSArray< CLPlacemark *> *__nullable placemarks, NSError *__nullable error) {
+            
+            [GeocodingHandler completeGeocodingWith:placemarks
+                                              error:error
+                                            success:successHandler
+                                            failure:failureHandler];
+            
+            if (locale != nil) {
+                [standardUserDefaults setValue:defaultLanguages
+                                        forKey:@"AppleLanguages"];
+            }
+        }];
+    }
+    
+    return;
+}
+
+- (void) geocodeToAddress: (CLLocation *)location
+                   locale: (NSLocale *)locale
+                  success: (GeocodingSuccess)successHandler
+                  failure: (GeocodingFailure)failureHandler  {
+    
+    if (@available(iOS 11.0, *)) {
+        [_geocoder reverseGeocodeLocation:location
+                          preferredLocale:locale
+                        completionHandler:^(NSArray< CLPlacemark *> *__nullable placemarks, NSError *__nullable error) {
+            [GeocodingHandler completeGeocodingWith:placemarks
+                                              error:error
+                                            success:successHandler
+                                            failure:failureHandler];
+        }];
+        
+    } else {
+        NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
+        NSArray<NSString * > *defaultLanguages;
+        
+        if (locale != nil) {
+            defaultLanguages = [standardUserDefaults arrayForKey:@"AppleLanguages"];
+            [standardUserDefaults setValue:[GeocodingHandler languageCode:locale]
+                                    forKey:@"AppleLanguages"];
+        }
+        
+        [_geocoder reverseGeocodeLocation:location
+                        completionHandler:^(NSArray< CLPlacemark *> *__nullable placemarks, NSError *__nullable error) {
+            [GeocodingHandler completeGeocodingWith:placemarks
+                                              error:error
+                                            success:successHandler
+                                            failure:failureHandler];
+            
+            if (locale != nil) {
+                [standardUserDefaults setValue:defaultLanguages forKey:@"AppleLanguages"];
+            }
+        }];
+    }
+}
+
++ (void) completeGeocodingWith: (NSArray<CLPlacemark *> *) placemarks
+                         error: (NSError *) error
+                       success: (GeocodingSuccess)successHandler
+                       failure: (GeocodingFailure) failureHandler {
+    if (error != nil) {
+        failureHandler(@"IO_ERROR", error.description);
+    } else if (placemarks == nil) {
+        failureHandler(@"NOT_FOUND", @"Could not find any result for the supplied address or coordinates.");
+    } else {
+        successHandler(placemarks);
+    }
+}
+
+
++ (NSString *) languageCode:(NSLocale *)locale {
+    if (@available(iOS 10.0, *)) {
+        return [locale languageCode];
+    } else {
+        return [[locale localeIdentifier] substringToIndex:2];
+    }
+}
+@end
