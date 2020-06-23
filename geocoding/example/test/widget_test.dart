201e1d65db451917a7c4d47e58fcd5ba6f9659b1
danielroek <daniel.roek@gmail.com>
2020-06-23 16:42:06 +0200

    Implemented Baseflow example app template

--- geocoding/example/test/widget_test.dart
+++ geocoding/example/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:geocoding_example/main.dart';
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(BaseflowPluginExample());
 
     // Verify that platform version is retrieved.
     expect(
