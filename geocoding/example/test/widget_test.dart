e15ba7d6dbc345304421e8b796858e0d461da65d
danielroek <daniel.roek@gmail.com>
2020-06-23 17:09:47 +0200

    Removed test from example app

--- geocoding/example/test/widget_test.dart
+++ geocoding/example/test/widget_test.dart
@@ -10,18 +10,4 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:geocoding_example/main.dart';
 
-void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(BaseflowPluginExample());
-
-    // Verify that platform version is retrieved.
-    expect(
-      find.byWidgetPredicate(
-        (Widget widget) =>
-            widget is Text && widget.data.startsWith('Running on:'),
-      ),
-      findsOneWidget,
-    );
-  });
-}
+void main() {}
