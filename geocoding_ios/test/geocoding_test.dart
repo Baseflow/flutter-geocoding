1b9426843710c482575b41f89633ef5d09a378e5
Paul Staats <paul@pepper-nuts.nl>
2023-03-01 10:54:49 +0100

    Migrated the geocoding to a federated plugin

---
+++ geocoding_ios/test/geocoding_test.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:geocoding_ios/geocoding_ios.dart';
+import 'package:geocoding_platform_interface/geocoding_platform_interface.dart';
+import 'package:mockito/mockito.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+final mockLocation = Location(
+  latitude: 52.2165157,
+  longitude: 6.9437819,
+  timestamp: DateTime.fromMillisecondsSinceEpoch(0).toUtc(),
+);
+
+final mockPlacemark = Placemark(
+    administrativeArea: 'Overijssel',
+    country: 'Netherlands',
+    isoCountryCode: 'NL',
+    locality: 'Enschede',
+    name: 'Gronausestraat',
+    postalCode: '',
+    street: 'Gronausestraat 710',
+    subAdministrativeArea: 'Enschede',
+    subLocality: 'Enschmarke',
+    subThoroughfare: '',
+    thoroughfare: 'Gronausestraat');
+
+void main() {
+  group('Geocoding', () {
+    setUp(() {
+      GeocodingPlatform.instance = MockGeocodingPlatform();
+    });
+
+    test('locationFromAddress', () async {
+      final geocoding = GeocodingIOS();
+      final locations = await (geocoding.locationFromAddress(''));
+      expect(locations.single, mockLocation);
+    });
+
+    test('placemarkFromCoordinates', () async {
+      final geocoding = GeocodingIOS();
+      final placemarks = await (geocoding.placemarkFromCoordinates(0, 0));
+      expect(placemarks.single, mockPlacemark);
+    });
+  });
+}
+
+class MockGeocodingPlatform extends Mock
+    with
+        // ignore: prefer_mixin
+        MockPlatformInterfaceMixin
+    implements
+        GeocodingPlatform {
+  @override
+  Future<List<Location>> locationFromAddress(
+    String address, {
+    String? localeIdentifier,
+  }) async {
+    return [mockLocation];
+  }
+
+  @override
+  Future<List<Placemark>> placemarkFromCoordinates(
+    double latitude,
+    double longitude, {
+    String? localeIdentifier,
+  }) async {
+    return [mockPlacemark];
+  }
+}
