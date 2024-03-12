c5a5abb86159537a9acec679ae92218539825f0b
tim Hoogstrate <tim566@hotmail.com>
2024-03-12 12:41:00 +0100

    updates documentation isPresent()

--- geocoding/lib/geocoding.dart
+++ geocoding/lib/geocoding.dart
@@ -45,18 +45,10 @@ Future<List<Placemark>> placemarkFromCoordinates(
       longitude,
     );
 
-/// Returns a list of [Location] instances found for the supplied address.
+/// Returns true if there is a geocoder implementation present that may return results.
+/// If true, there is still no guarantee that any individual geocoding attempt will succeed.
 ///
-/// In most situations the returned list should only contain one entry.
-/// However in some situations where the supplied address could not be
-/// resolved into a single [Location], multiple [Location] instances may be
-/// returned.
 ///
-/// Optionally you can specify a locale in which the results are returned.
-/// When not supplied the currently active locale of the device will be used.
-/// The `localeIdentifier` should be formatted using the syntax:
-/// [languageCode]_[countryCode] (eg. en_US or nl_NL).
-Future<bool> isPresent({
-  String? localeIdentifier,
-}) =>
-    GeocodingPlatform.instance!.isPresent();
+/// This method is only implemented on Android, calling this on iOS always
+/// returns [true].
+Future<bool> isPresent() => GeocodingPlatform.instance!.isPresent();
