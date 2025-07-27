b7d69bcb845ccc7568e95ace3a0a4f4e2e459b01
OutdatedGuy <everythingoutdated@gmail.com>
2025-07-28 04:25:14 +0530

    refactor: renamed geocoding_ios dir to geocoding_darwin

--- geocoding_darwin/example/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
+++ geocoding_darwin/example/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -15,18 +15,11 @@ let package = Package(
         .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
     ],
     dependencies: [
-        .package(name: "url_launcher_ios", path: "/Users/maurits/.pub-cache/hosted/pub.dev/url_launcher_ios-6.3.3/ios/url_launcher_ios"),
-        .package(name: "integration_test", path: "/Users/maurits/development/flutter/packages/integration_test/ios/integration_test"),
-        .package(name: "geocoding_darwin", path: "/Users/maurits/sources/Baseflow/Internal/Flutter/geocoding/geocoding_darwin/darwin/geocoding_darwin")
+        
     ],
     targets: [
         .target(
-            name: "FlutterGeneratedPluginSwiftPackage",
-            dependencies: [
-                .product(name: "url-launcher-ios", package: "url_launcher_ios"),
-                .product(name: "integration-test", package: "integration_test"),
-                .product(name: "geocoding-darwin", package: "geocoding_darwin")
-            ]
+            name: "FlutterGeneratedPluginSwiftPackage"
         )
     ]
 )
